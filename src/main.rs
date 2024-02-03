2e44367ca7bd3e8694ca0f7bb37a27274957fc7d
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-02-02 22:16:51 -0800

    Countdown and stats

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
-use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, Write};
 use random_word::Lang;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     print!("Enter number of words: ");
     io::stdout().flush().expect("Failed to flush stdout");
     // Ask user input, how many words they want
-    let mut times = String::new();
+    let mut word_count = String::new();
     io::stdin()
-        .read_line(&mut times)
+        .read_line(&mut word_count)
         .expect("Failed to read line");
-    let times: u32 = times.trim().parse().expect("Please type a number");
-    let mut count: u32 = times;
-    // Print random words as many times as user req
+    let word_count: u32 = word_count.trim().parse().expect("Please type a number");
+    let mut count: u32 = word_count;
+    // Print random words as many word_count as user req
     let mut words: Vec<&str> = Vec::new(); 
     while count > 0 {
         let rand = random_word::gen_len(5, Lang::En);
@@ -25,6 +26,8 @@ fn main() {
     for (i, word) in words.iter().enumerate() {
         println!("{i}: {word}");
     }
+    // Countdown timer until words disappear
+    countdown(word_count * 5);
     // Let user type the words they remembered
     print!("Enter the words you remember, separated by spaces: ");
     io::stdout().flush().expect("Failed to flush stdout");
@@ -33,13 +36,33 @@ fn main() {
     io::stdin()
         .read_line(&mut args)
         .expect("Failed to read line");
-    let args: Vec<&str> = args.split_whitespace().collect();
+    let guesses: Vec<&str> = args.split_whitespace().collect();
     // Count points
-    for (i, arg) in args.iter().enumerate() {
-        if let Some(pos) = words.iter().position(|x| *x == *arg) {
+    for (_i, guess) in guesses.iter().enumerate() {
+        if let Some(pos) = words.iter().position(|x| *x == *guess) {
             words.remove(pos);
             points += 1;
         }
     }
-    println!("You got {points}/{times} points!")
+    let score = 100 * points / word_count;
+    println!("{points}/{word_count} words - that's a score of {score}%!");
+    print!("Guessed: {args}");
+    print!("Missed:");
+    for s in words {
+        print!(" {s}");
+    }
+}
+
+fn countdown(countdown_seconds: u32) {
+    println!("Starting countdown:");
+    for i in (1..=countdown_seconds).rev() {
+        println!("{} seconds remaining...", i);
+        print!("\x1B[2K"); // Clear line
+        print!("\x1B[1A"); // Move the cursor up one line
+        print!("\x1B[2K"); // Clear line
+        thread::sleep(Duration::from_secs(1));
+    }
+    println!("Time's up!");
+    print!("\x1B[2J\x1B[1;1H");
+    io::stdout().flush().unwrap();
 }
